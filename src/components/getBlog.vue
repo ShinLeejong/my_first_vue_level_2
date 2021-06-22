22dff2b21dc4d6472f6d598040a4ceba217c4d33
iiy4383 <znlspf123@naver.com>
2021-06-22 12:31:56 +0900

    20210622 Add input:text and search button on getBlog.vue

--- src/components/getBlog.vue
+++ src/components/getBlog.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="get_blog">
         <h2>불러온 포스팅</h2>
-        <div v-for="post in posts" v-bind:key="post" class="dropped_blog">
+        <div id="search">
+            <input type="text" id="search-text" v-model="search" placeholder="검색할 단어를 입력하세요" />
+            <button v-on:click="onSearchClick">검색</button>            
+        </div>
+        <div v-for="post in posts" v-bind:key="post.title" class="dropped_blog">
             <h3 v-randomColor>{{post.title | uppercase}}</h3>
             <article>{{post.body | slice}}</article>
         </div>
@@ -11,22 +15,28 @@
 export default {
     data() {
         return {
-            posts: []
+            posts: [],
+            originalposts: [],
+            search: ''
         }
     },
     methods: {
-
+        onSearchClick: function() {
+            if(this.search === '') this.posts = this.originalposts;
+            this.posts = this.posts.filter(e => e.title.includes(this.search));
+        }
     },
     // life-cycle method
     created() {
         this.$http.get("https://jsonplaceholder.typicode.com/posts")
         .then(data => {
             this.posts = data.body.slice(0, 10);
-            console.log(this.posts);
+            this.originalposts = this.posts;
         }).catch(err => console.error(err))
     }
 }
 </script>
+
 <style scoped>
     .dropped_blog {
         margin: 0 auto;
@@ -41,6 +51,10 @@ export default {
 
     .dropped_blog > article {
         opacity: 0.
+    }
+
+    #search > * {
+        margin: 0 .5rem 1rem;
     }
 
 </style>
